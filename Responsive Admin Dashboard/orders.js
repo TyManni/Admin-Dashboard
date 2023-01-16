711a4cc84b7cd92446308432269431da68e2ec07
Tyara Manning <94203950+TyManni@users.noreply.github.com>
2023-01-15 23:33:35 -0600

    Add files via upload

---
+++ Responsive Admin Dashboard/orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+  {
+    productName: "Soul Candy",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Soul Candy",
+    productNumber: "36378",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+  {
+    productName: "Soul Candy",
+    productNumber: "49347",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Soul Candy",
+    productNumber: "96996",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "Soul Candy",
+    productNumber: "22812",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+];
